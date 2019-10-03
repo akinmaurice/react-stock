6ed1904088dc373b2c0406cc569ff42d13ecf563
Akinyemi Akintayo <akinyemiakintayo@Akinyemis-MacBook-Pro.local>
2019-10-03 15:53:19 +0100

    Fix error when Fetching data from api

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,32 +1,43 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import * as d3 from 'd3';
 import Header from './layout/Header';
 
-const url = 'https://www.quandl.com/api/v3/datasets/OPEC/ORB.json';
+const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=MSFT&apikey=demo';
 function Stock() {
   const [ isLoading, setLoading ] = useState(false);
-  const [ data, setData ] = useState([]);
+  const [ xData, setXData ] = useState([]);
+  const [ yData, setYData ] = useState([]);
   const [ isError, setError ] = useState(false);
 
-  useEffect(() => {
+  const canvasRef = useRef('canvas');
+
+
+  const fetchData = () => {
     setLoading(true);
-    console.log('calling effect');
     axios.get(url)
-    .then((response)=>{
-      const { data: dataset } = response;
-      const { data } = dataset.dataset;
-      const totalLength = data.length;
-      const limit = 100;
-      const unused = totalLength - limit;
-      data.splice(limit, unused);
-      setData(data);
-    })
-    .catch((e)=>{
-      setError(true);
-      setLoading(false);
-      console.log(e)
-    })
+    .then((response)=>
+    {
+      let stockChartXValues = [];
+      let stockChartYValues = [];
+        const { data } = response;
+        for(let key in data['Time Series (Daily)']) {
+          stockChartXValues.push(key);
+          stockChartYValues.push(data['Time Series (Daily)'][key]['1. open']);
+        }
+        setXData(stockChartXValues);
+        setYData(stockChartYValues);
+        setLoading(false);
+      })
+      .catch((e)=>{
+        setError(true);
+        setLoading(false);
+        console.log(e)
+      })
+  }
+
+  useEffect(() => {
+    fetchData()
   }, [])
 
   let view = '';
@@ -36,28 +47,16 @@ function Stock() {
   if(isError) {
     view = 'Could not load Data. Please try again';
   }
-  if(data.length > 0) {
-    const minX = d3.min(data.map(o => o[0]));
-    const maxX = d3.min(data.map(o => o[0]));
-    const minY = d3.min(data.map(o => o[1]));
-    const maxY = d3.min(data.map(o => o[1]));
-
-    let x = d3
-    .scaleLinear()
-    .domain([minX, maxX])
-    .range([0, 300]);
-
-    let y = d3
-    .scaleLinear()
-    .domain([minY, maxY])
-    .range([100, 100/3]);
-
+  if(xData.length > 0) {
     view = 'Got Data! Hello';
   }
   return (
-    <div>
+    <div className="text-center">
       <Header />
-      {view}
+      <br />
+      <div ref={canvasRef}>
+        {view}
+      </div>
     </div>
   );
 }
